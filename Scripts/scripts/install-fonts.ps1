f1b6c5280898a20b93fc9a8c1e08a8bf68c984e1
meisim <simmei@protonmail.com>
2016-10-20 08:24:26 +0200

    add script to install fonts

---
+++ Scripts/scripts/install-fonts.ps1
@@ -0,0 +1,12 @@
+﻿# install fonts
+$Fonts = 0x14
+$Path = "C:\Scripts\fonts"
+$objShell = New-Object -ComObject Shell.Application
+$objFolder = $objShell.Namespace($Fonts)
+#New-Item $Path -type directory
+#Copy-Item "f:\NewFontsForDeployment\*" $Path
+$Fontdir = dir $Path
+foreach($File in $Fontdir) {
+  $objFolder.CopyHere($File.fullname)
+}
+remove-item $Path -recurse
