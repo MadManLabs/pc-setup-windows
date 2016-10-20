55af404538c8d16c1aaed30079b51a595d42f802
meisim <simmei@protonmail.com>
2016-10-20 15:24:07 +0200

    add mkdir

--- Scripts/scripts/cust-office-settings.ps1
+++ Scripts/scripts/cust-office-settings.ps1
@@ -1,10 +1,13 @@
 #Block signing into Office
+mkdir -Force "HKCU:\software\policies\microsoft\office\16.0\common\signin"
 Set-ItemProperty "HKCU:\software\policies\microsoft\office\16.0\common\signin" "signinoptions" 3
 #Don't show OneDrive Sign In
+mkdir -Force "HKCU:\software\policies\microsoft\office\16.0\common\general"
 Set-ItemProperty "HKCU:\software\policies\microsoft\office\16.0\common\general" "skydrivesigninoption" 0
 #Disable the Office Start screen for all Office applications
 Set-ItemProperty "HKCU:\software\policies\microsoft\office\16.0\common\general" "disableboottoofficestart" 1
 #Disable including screenshot with Office Feedback
+mkdir -Force "HKCU:\software\policies\microsoft\office\16.0\common\feedback"
 Set-ItemProperty "HKCU:\software\policies\microsoft\office\16.0\common\feedback" "includescreenshot" 0
 #Disable automatically receive small updates to improve reliability
 Set-ItemProperty "HKCU:\software\policies\microsoft\office\16.0\common" "updatereliabilitydata" 0
@@ -15,6 +18,7 @@ Set-ItemProperty "HKCU:\software\policies\microsoft\office\16.0\common\feedback"
 #Disable send personal information
 Set-ItemProperty "HKCU:\software\policies\microsoft\office\16.0\common" "sendcustomerdata" 0
 #Turn off telemetry data collection
+mkdir -Force "HKCU:\software\policies\microsoft\office\16.0\osm"
 Set-ItemProperty "HKCU:\software\policies\microsoft\office\16.0\osm" "enablelogging" 0
 #Hide the Learn more about SharePoint Hyperlink
 Set-ItemProperty "HKCU:\software\policies\microsoft\office\16.0\common\sharepointintegration" "hidelearnmorelink" 1
